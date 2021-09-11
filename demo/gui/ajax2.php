cf0db37fb0ac757cdf0d10328713da9526345bb8
Minh Pham <smpleader@gmail.com>
2021-09-11 13:31:05 +0700

    add sample for multi level

---
+++ demo/gui/ajax2.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../include.php'; 
+
+// Declare
+$level = isset($_GET['level']) ? (int)$_GET['level'] : 1;
+$process = new ProcessFile2($level);
+$process->setArray( demo::sampleArray() );
+
+$sequence = new SequencyProcess\Sequence($process, 2);
+
+// Process each segment
+$start = isset($_GET['start']) ? (int)$_GET['start'] : 0;
+$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 5;
+$output = $sequence->run($start, $limit); 
+
+// Output json result
+http_response_code(200);
+header('Content-Type: application/json;charset=utf-8');
+echo json_encode($output);
+
+die;
