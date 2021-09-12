308db7fb01a8fab31603f2ab3d7521004ac18430
Minh Pham <smpleader@gmail.com>
2021-09-12 09:56:33 +0700

    add Cli support

--- src/ProcessFile.php
+++ src/ProcessFile.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Sequency Process - Class Process by an array use state from a file
+ * Sequency Process - Class Process by an array use data from a file
  * 
  * @project: https://github.com/smpleader/Sequency-process
  * @author: Pham Minh - smpleader
@@ -12,38 +12,6 @@ namespace SequencyProcess;
 
 class ProcessFile extends ProcessArray
 {
-	protected $statePath;
-
-	public function setStatePath(string $path)
-	{
-		$this->statePath = $path;
-		if(!file_exists($this->statePath))
-		{
-			$try = fopen($this->statePath, 'a+');
-			if(false === $try)
-			{
-				$this->setStatePath( DEMO_PATH. 'state.log' );
-			}
-		}
-	}
-
-    public function loadState()
-	{
-		$try = file_get_contents($this->statePath);
-
-		if(false === $try)
-		{
-			$this->error = 'Invalid file '. $path;
-			$state = ['start'=>0];
-		}
-		else
-		{
-			$state = (array) json_decode($try);
-		}
-
-		return $state;
-	}
-
     public function loadData(string $path)
 	{
 		$try = file_get_contents($path);
@@ -77,14 +45,4 @@ class ProcessFile extends ProcessArray
 			$this->error = $e->getMessage() ;
 		}
 	}
-
-	public function finished()
-	{
-		if ( 'cli' == php_sapi_name())
-		{
-			$res = $this->result->output();
-			file_put_contents($this->statePath, json_encode($res));
-		}
-	}
-
 }
---
+++ src/ProcessFileCli.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Sequency Process - Class Process by an array use state from a file
+ * 
+ * @project: https://github.com/smpleader/Sequency-process
+ * @author: Pham Minh - smpleader
+ * @description: Class to process the sequency of an array from a file
+ * 
+ */
+
+namespace SequencyProcess;
+
+class ProcessFile extends ProcessFileCli
+{
+	protected $statePath;
+
+	public function setStatePath(string $path)
+	{
+		$this->statePath = $path;
+		if(!file_exists($this->statePath))
+		{
+			$try = fopen($this->statePath, 'a+');
+			if(false === $try)
+			{
+				$this->setStatePath( DEMO_PATH. 'state.log' );
+			}
+		}
+	}
+
+    public function loadState()
+	{
+		$try = file_get_contents($this->statePath);
+
+		if(false === $try)
+		{
+			$this->error = 'Invalid file '. $path;
+			$state = ['start'=>0];
+		}
+		else
+		{
+			$state = (array) json_decode($try);
+		}
+
+		return $state;
+	}
+	
+	public function finished()
+	{
+		if ( 'cli' == php_sapi_name())
+		{
+			$res = $this->result->output();
+			file_put_contents($this->statePath, json_encode($res));
+		}
+	}
+}
